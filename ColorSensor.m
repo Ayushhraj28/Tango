3c9424b65cc2e21168fa4651e9f309b224d25432
Ayush Raj <151850500+Ayushhraj28@users.noreply.github.com>
2024-11-07 20:15:39 -0700

    Update ColorSensor.m

--- ColorSensor.m
+++ ColorSensor.m
@@ -1,24 +1,38 @@
-brick.SetColorMode(2, 2);
+brick.SetColorMode(2, 2);  
+
 while 1
     color = brick.ColorCode(2);
 
-    if color == 1
-        brick.MoveMotor('A', 50);
+    if color == 5  
+        brick.StopMotor(‘AD');
+        pause(1);  
+
+	brick.MoveMotor('A', 50);
         brick.MoveMotor('D', 50);
 
-    elseif color == 5
-        brick.MoveMotor('A', 0);
-        brick.MoveMotor('D', 0);
     elseif color == 2
-        brick.MoveMotor('A', 0);
-        brick.MoveMotor('D', 0);
+        brick.StopMotor(‘AD');
+
         brick.playTone(50, 3000, 1000);
+        pause(1);  
         brick.playTone(50, 3000, 1000);
+
+	break;
+
     elseif color == 3
-        brick.MoveMotor('A', 0);
-        brick.MoveMotor('D', 0);
+         brick.StopMotor(‘AD');
+
         brick.playTone(50, 3000, 1000);
+        pause(1);  
         brick.playTone(50, 3000, 1000);
+        pause(1);  
         brick.playTone(50, 3000, 1000);
+
+	brick.MoveMotor('A', 50);
+        brick.MoveMotor('D', 50);
+
+    else
+        brick.MoveMotor('A', 50);
+        brick.MoveMotor('D', 50);
     end
 end
